857ba6dbe7ab1a0912a42cde5635c3a010e1c95c
unknown <akshita.laddha27@gmail.com>
2026-07-20 19:16:37 +0530

    adding changes

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-def longest_valid_parentheses(s: str) -> int:
-    max_len = 0
-    stack = [-1]  
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        max_len = 0
+        stack = [-1]
 
-    for i, c in enumerate(s):
-        if c == '(':
-            stack.append(i)
-        else:
-            stack.pop()
-            if not stack:
+        for i, c in enumerate(s):
+            if c == '(':
                 stack.append(i)
             else:
-                max_len = max(max_len, i - stack[-1])
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_len = max(max_len, i - stack[-1])
 
-    return max_len
+        return max_len
 
 
 if __name__ == "__main__":
-    print(longest_valid_parentheses("(()"))     # 2
-    print(longest_valid_parentheses(")()())"))  # 4
-    print(longest_valid_parentheses(""))        # 0
+    solution = Solution()
+    print(solution.longestValidParentheses("(()"))     # 2
+    print(solution.longestValidParentheses(")()())"))  # 4
+    print(solution.longestValidParentheses(""))        # 0
